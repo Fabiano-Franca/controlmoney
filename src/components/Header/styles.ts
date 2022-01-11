3a2aec41ac73792bd513ab83fa744f8ca32de352
Fabiano-Franca <fabianofranca.ti@gmail.com>
2022-01-11 18:40:32 -0300

    Modal em construção

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+    background: var(--blue);
+`;
+
+export const Content = styled.div`
+    max-width: 1120px;
+    margin: 0 auto; //centraliza a div
+
+    padding: 2rem 1rem 12rem;
+    display: flex;  //Alinha a logo e o botão
+    align-items: center; //Alinha a logo e o botão
+    justify-content: space-between; //Justifica com um espaço entre os elementos, colocando umna ponta e outro na outra ponta
+
+    button {
+        font-size: 1rem;
+        color: #FFF;
+        background: var(--blue-ligth);
+        border: 0;
+        padding: 0 2rem;
+        border-radius: 0.25rem;
+        height: 3rem;
+        
+        transition: filter 0.2s; //Seta a duração do efeito no filter
+
+        &:hover {
+            filter: brightness(0.9); //Escurece o botão e todo seu conteudo ao passar o mouse
+        }
+    }
+
+`;
---
+++ src/styles/global.ts
@@ -0,0 +1,93 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    //Define as variáveis globais
+    :root {
+        --background: #f0f2f5;
+        --red: #e52e4d;
+        --green: #33CC95;
+        --blue: #5429cc;
+
+        --blue-ligth: #6933ff;
+
+        --text-title: #363f5f;
+        --text-body: #969cb3;
+
+        --background: #f0f2f5;
+        --shape: #FFFFFF;
+    }
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    //Por padrão a font do css é font-size: 16px (Desktop)
+    //REM = 1rem = font-size (16px)
+    html {
+        //Quando o usuario estiver com uma tela de até 1080px utilize % da fonte padrão
+        @media (max-width: 1080px) {
+            font-size: 93.75%; //15px
+        }
+
+        //Quando estiver em uma tela menor do que  720px utilize % da fonte padrão
+        @media (max-width: 720px) {
+            font-size: 97.5%; //14px
+        }
+    }
+
+
+    body {
+        background: var(--background);
+        -webkit-font-smoothing: antialiased;
+    }
+
+    //Os elementos input, textarea e button por padrão não importa as fontes do body.
+    body, input, textarea, button {
+        font-family: 'Poppins', sans-serif;
+        font-weight: 400;
+    }
+
+    //Fontes de negrito
+    h1, h2, h3, h4, h5, h6, strong {
+        font-weight: 600;
+    }
+
+    //O cursor do mouse ficará como um ponteiro nos botões
+    button {
+        cursor: pointer;
+    }
+
+    //Para componentes desabilitados ficará mais claro e terá a placa de desabilitado.
+    [disabled] {
+        opacity: 0.6;
+        cursor: not-allowed;
+    }
+
+    //Estilo do fundo do modal (área cinza)
+    .react-modal-overlay {
+        background: rgba(0, 0, 0, 0.5);
+
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        right: 0;
+        left: 0;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    
+    .react-modal-content {
+        width: 100%;
+        max-width: 576px;
+        background: var(--background);
+        padding: 3rem;
+        position: relative;
+        border-radius: 0.24rem;
+    }
+
+
+`
